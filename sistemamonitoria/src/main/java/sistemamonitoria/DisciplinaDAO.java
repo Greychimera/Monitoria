7d77d3d8588de651a2255fcf6d634f07dd878336
Jociele <jociellesouzab@gmail.com>
2016-09-20 12:06:01 -0300

    Modificação na classe DisciplinaDAO.java0 e criação da classe CadastroDisciplina.java

--- sistemamonitoria/src/main/java/sistemamonitoria/DisciplinaDAO.java
+++ sistemamonitoria/src/main/java/sistemamonitoria/DisciplinaDAO.java
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 package sistemamonitoria;
-
+import hibernate.HibernateUtil;
+import java.util.List; 
+import org.hibernate.HibernateException; 
+import org.hibernate.Session; 
+import org.hibernate.Transaction;
 /**
  *
  * @author aluno
  */
 public class DisciplinaDAO {
-    
+    public boolean addDisciplina(Disciplina disciplina){
+       
+      Session session = HibernateUtil.abrirSessaoComBD();
+      Transaction tx = null;
+      Integer disciplinaID = null;
+      boolean foi = false;
+      
+      
+      try{
+         tx = session.beginTransaction();
+         
+         disciplinaID = (Integer) session.save(disciplina); 
+         tx.commit();
+         
+      }catch (HibernateException e) {
+          
+         if (tx!=null){
+             tx.rollback();
+         }
+         throw e;
+         
+      }finally {
+          
+         session.close(); 
+         
+      }
+      
+      if (disciplinaID != null){
+ 
+       foi= true;
+        }
+      return foi;
+      
+   }
+   
 }
